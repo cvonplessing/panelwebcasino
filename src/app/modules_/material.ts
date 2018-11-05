238dea103e684765136f63fdd26a9dc9587d43f6
nphellim <nphellim@gmail.com>
2018-11-04 21:55:44 -0300

    botones editar

--- src/app/modules_/material.ts
+++ src/app/modules_/material.ts
@@ -1,8 +1,9 @@
-import { MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule} from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule],
-    exports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule],
+    imports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
+    exports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
 })
 export class Material { }
+1
