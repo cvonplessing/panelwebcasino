03646b523c0dc92de5ec7c5aa1556b8e361af4cc
cvonplessing <cvonplessing@udec.cl>
2018-11-06 20:41:07 -0300

    Partiendo comp editar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { PlatosComponent } from './platos/platos.component';
 import { LoginComponent } from './login/login.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { Material } from './modules_/material';
 import { SenderComponent } from './sender/sender.component';
+import { EditarComponent } from './editar/editar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlatosComponent,
     LoginComponent,
-    SenderComponent
+    SenderComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
     Material,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
