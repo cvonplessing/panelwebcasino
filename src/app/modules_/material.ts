a7eb68d185a85d707dabbebfc463e79135b23375
cvonplessing <cvonplessing@udec.cl>
2018-11-06 23:38:19 -0300

    intento de editor v2

--- src/app/modules_/material.ts
+++ src/app/modules_/material.ts
@@ -1,9 +1,9 @@
-import { MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule} from '@angular/material';
+import { MatAutocompleteModule, MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
-    exports: [MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
+    imports: [MatAutocompleteModule, MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
+    exports: [MatAutocompleteModule, MatToolbarModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatExpansionModule, MatCardModule, MatTableModule],
 })
 export class Material { }
 1
