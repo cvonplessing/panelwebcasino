fb8ed18e11c0c59e7358450f3378fa32fc3e7057
cvonplessing <cvonplessing@udec.cl>
2018-11-12 16:32:40 -0300

    tool bar separado de platos, agregado un botoncito para volver a platos

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Casino } from '../models_/casino';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+    defaultCasino: Casino = new Casino(0, 'Ombligo');
   constructor() { }
 
   ngOnInit() {
   }
-
 }
